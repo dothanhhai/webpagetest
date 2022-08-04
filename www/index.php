7c3a909cc68427e4dea57d7204a57987647fe98f
Hai Do <hai.do@niteco.se>
2022-08-04 17:26:36 +0700

    remove access index.php

--- www/index.php
+++ www/index.php
@@ -1,5 +1,8 @@
 <?php
-
+if(empty($_GET['niteco']) || $_GET['niteco'] != 'wpt') {
+    header("HTTP/1.0 404 Not Found");
+    die;
+}
 // Copyright 2020 Catchpoint Systems Inc.
 // Use of this source code is governed by the Polyform Shield 1.0.0 license that can be
 // found in the LICENSE.md file.
