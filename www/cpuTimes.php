4b6773c2437a7aee05b900b79608a446f2fe0977
Hai Do <hai.do@niteco.se>
2023-03-24 14:27:43 +0700

    correct cpuTimes values

--- www/cpuTimes.php
+++ www/cpuTimes.php
@@ -46,26 +46,91 @@ $data['utilization'] = LoadPerfData($localPaths->utilizationFile(), true, true, 
 
 // Calculate the timeline-based CPU times
 $cpu_slices = DevToolsGetCPUSlicesForStep($localPaths);
+$reduceBlock = 10000 / $cpu_slices['slice_usecs'];
+$cpuTypes = cpuTypes();
 
 if ($cpu_slices['slices'][$cpu_slices['main_thread']]) {
     $mainThread = $cpu_slices['slices'][$cpu_slices['main_thread']];
     $data['mainThread']['miliseconds'] = round($cpu_slices['total_usecs'] / 1000);
-    $reduceBlock = 10000 / $cpu_slices['slice_usecs'];
     foreach ($mainThread as $key => $items) {
         foreach ($items as $k => $v) {
             if ($v > 0) {
                 $timeBlock = floor($k / $reduceBlock);
-                if (!isset($data['mainThread']['data']["$timeBlock"]) || $data['mainThread']['data']["$timeBlock"]['max'] < $v) {
-                    $data['mainThread']['data']["$timeBlock"] = [$key => $v, 'max' => $v];
+                $cpuType = $key;
+                if (!isset($cpuTypes[$key])) {
+                    $cpuType = 'Other';
                 }
+                if (empty($data['mainThread']['data']["$timeBlock"][$cpuType])) {
+                    $data['mainThread']['data']["$timeBlock"][$cpuType] = 0;
+                }
+                $data['mainThread']['data']["$timeBlock"][$cpuType] += $v;
             }
+        }
+    }
+    foreach ($data['mainThread']['data'] as $timeBlock => $items) {
+        foreach ($items as $k => $v) {
+            $data['mainThread']['data'][$timeBlock][$k] = round($data['mainThread']['data'][$timeBlock][$k] / $reduceBlock);
         }
     }
 }
 
-foreach ($data['mainThread']['data'] as $k => $v) {
-    unset($data['mainThread']['data'][$k]['max']);
-}
-
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($data);
+
+function cpuTypes()
+{
+    $cpuTypes = array();
+
+    $cpuTypes['ParseHTML'] = true;
+    $cpuTypes['ResourceReceivedData'] = true;
+    $cpuTypes['ResourceSendRequest'] = true;
+    $cpuTypes['ResourceReceivedResponse'] = true;
+    $cpuTypes['ResourceReceiveResponse'] = true;
+    $cpuTypes['ResourceFinish'] = true;
+    $cpuTypes['CommitLoad'] = true;
+
+    $cpuTypes['Layout'] = true;
+    $cpuTypes['RecalculateStyles'] = true;
+    $cpuTypes['ParseAuthorStyleSheet'] = true;
+    $cpuTypes['ScheduleStyleRecalculation'] = true;
+    $cpuTypes['InvalidateLayout'] = true;
+    $cpuTypes['UpdateLayoutTree'] = true;
+
+    $cpuTypes['Paint'] = true;
+    $cpuTypes['PaintImage'] = true;
+    $cpuTypes['PaintSetup'] = true;
+    $cpuTypes['CompositeLayers'] = true;
+    $cpuTypes['DecodeImage'] = true;
+    $cpuTypes['Decode Image'] = true;
+    $cpuTypes['ImageDecodeTask'] = true;
+    $cpuTypes['Rasterize'] = true;
+    $cpuTypes['GPUTask'] = true;
+    $cpuTypes['SetLayerTreeId'] = true;
+    $cpuTypes['layerId'] = true;
+    $cpuTypes['UpdateLayer'] = true;
+    $cpuTypes['UpdateLayerTree'] = true;
+    $cpuTypes['Draw LazyPixelRef'] = true;
+    $cpuTypes['Decode LazyPixelRef'] = true;
+
+    $cpuTypes['EvaluateScript'] = true;
+    $cpuTypes['EventDispatch'] = true;
+    $cpuTypes['FunctionCall'] = true;
+    $cpuTypes['GCEvent'] = true;
+    $cpuTypes['TimerInstall'] = true;
+    $cpuTypes['TimerFire'] = true;
+    $cpuTypes['TimerRemove'] = true;
+    $cpuTypes['XHRLoad'] = true;
+    $cpuTypes['XHRReadyStateChange'] = true;
+    $cpuTypes['v8.compile'] = true;
+    $cpuTypes['MinorGC'] = true;
+    $cpuTypes['MajorGC'] = true;
+    $cpuTypes['FireAnimationFrame'] = true;
+    $cpuTypes['ThreadState::completeSweep'] = true;
+    $cpuTypes['Heap::collectGarbage'] = true;
+    $cpuTypes['ThreadState::performIdleLazySweep'] = true;
+
+    $cpuTypes['Other'] = true;
+    $cpuTypes['Program'] = true;
+
+    return $cpuTypes;
+}
