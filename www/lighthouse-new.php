6c0b7309ee08fc2a737d18cf0bcba700ce2a25c7
Hai Do <hai.do@niteco.se>
2023-03-27 14:04:33 +0700

    fix treemap of lighthouse

--- www/lighthouse-new.php
+++ www/lighthouse-new.php
@@ -79,6 +79,7 @@ if ($lhResults) {
     $treemap = [
         'lhr' => [
             'requestedUrl' => $url,
+            'finalUrl' => $url,
             'audits' => [
                 'script-treemap-data' => [
                     'details' => $lhResults->audits->{'script-treemap-data'}->details
